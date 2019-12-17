f2e7fb88bb7aeada89fb2cab516f039affbfcb6c
lyndsiWilliams <kcatgirl@gmail.com>
2019-12-17 07:56:12 -0600

    Dashboard and Controls testing complete, working on Display testing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,10 +1,23 @@
 // Test away
 import React from 'react';
-import {render} from '@testing-library/react';
-// import * as jest from '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 
 import Dashboard from './Dashboard';
 
+
+// TESTS BEGIN
+
+// Test: Component renders
 test("Dashboard renders without crashing", () => {
   render(<Dashboard />);
+});
+
+// Test: shows the controls and display
+test("Controls and Display rendering correctly", () => {
+  const { getByText } = render(<Dashboard />);
+  const controls = getByText(/Close Gate/i);
+  const display = getByText(/Unlocked/i);
+
+  expect(controls).toBeDefined();
+  expect(display).toBeDefined();
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,9 +1,31 @@
 // Test away!
 import React from 'react';
-import {render} from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Display from './Display';
 
+
+// TESTS BEGIN
+
+// Test: Component renders
 test("Display renders without crashing", () => {
   render(<Display />);
 });
+
+// Test: displays if gate is open/closed and if it is locked/unlocked
+test("Check open/closed and locked/unlocked display", () => {
+  const { getByText, getByTestId } = render(<Display />);
+  
+  getByText(/open/i);
+  getByText(/unlocked/i);
+  getByText(/locked/i);
+  getByTestId(/closed/i);
+});
+
+// Test: displays 'Closed' if the closed prop is true and 'Open' if otherwise
+
+// Test: displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise
+
+// Test: when locked or closed use the red-led class
+
+// Test: when unlocked or open use the green-led class
