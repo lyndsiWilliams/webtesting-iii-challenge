7564c5734981de10b1572b00710f14c9c85e928f
lyndsiWilliams <kcatgirl@gmail.com>
2019-12-17 07:07:54 -0600

    Each component has a successful render test, started on extended testing of Controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import {render} from '@testing-library/react';
+
+// Component
+import Controls from './Controls';
+
+test("Controls renders without crashing", () => {
+  render(<Controls />);
+});
+
+test("Gate are defaults at unlock and open", () => {
+    const {getByText} = render(<Controls locked closed />)
+
+    getByText('Unlock Gate' && 'Open Gate')
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
 // Test away
+import React from 'react';
+import {render} from '@testing-library/react';
+// import * as jest from '@testing-library/jest-dom';
+
+import Dashboard from './Dashboard';
+
+test("Dashboard renders without crashing", () => {
+  render(<Dashboard />);
+});
