0191395a842f3c27f923c5db35c72e3d29aad535
lyndsiWilliams <kcatgirl@gmail.com>
2019-12-17 08:05:12 -0600

    Finished Display testing, MVP achieved - 14 tests!

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,7 @@
 // Test away!
 import React from 'react';
 import { render } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 
 import Display from './Display';
 
@@ -23,9 +24,39 @@ test("Check open/closed and locked/unlocked display", () => {
 });
 
 // Test: displays 'Closed' if the closed prop is true and 'Open' if otherwise
+test("Displays 'Closed' if the closed prop is true and 'Open' if otherwise", () => {
+  const { getByText, getByTestId, rerender } = render(<Display closed />);
+  getByTestId(/closed/i);
+
+  rerender(<Display closed={false} />);
+  getByText(/open/i);
+});
 
 // Test: displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise
+test("Displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise", () => {
+  const { getByText, rerender } = render(<Display locked />);
+  getByText(/locked/i);
+
+  rerender(<Display locked={false} />);
+  getByText(/unlocked/i);
+});
 
 // Test: when locked or closed use the red-led class
+test("Use the red-led class when locked or closed", () => {
+  const { getByText } = render(<Display closed locked />);
+  const lock = getByText(/locked/i);
+  const close = getByText(/closed/i);
+
+  expect(lock).toHaveClass("red-led");
+  expect(close).toHaveClass("red-led");
+});
 
 // Test: when unlocked or open use the green-led class
+test("Use the green-led class when unlocked or open", () => {
+  const { getByText } = render(<Display closed={false} locked={false} />);
+  const lock = getByText(/unlocked/i);
+  const close = getByText(/open/i);
+
+  expect(lock).toHaveClass("green-led");
+  expect(close).toHaveClass("green-led");
+});
